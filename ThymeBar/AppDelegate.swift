e40a2cc1d79ba03f2073342b2ea00abe1ae85dea
Ian Ruh <ianruh2@gmail.com>
2018-10-21 09:59:41 -0500

    Initial Commit

---
+++ ThymeBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ThymeBar
+//
+//  Created by Ian Ruh on 10/21/18.
+//  Copyright © 2018 Ian Ruh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
